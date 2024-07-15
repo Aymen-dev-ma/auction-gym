bc410d94824df2ba3d6cd8ba002f9ba8121d6224
Aymen <aymen.nasri@evercot.ai>
2024-07-15 20:39:52 +0200

    Update BidderAllocation.py

--- src/BidderAllocation.py
+++ src/BidderAllocation.py
@@ -7,33 +7,25 @@ from tqdm import tqdm
 
 from Models import PyTorchLogisticRegression, sigmoid
 
-
 class Allocator:
-    """ Base class for an allocator """
-
     def __init__(self, rng):
         self.rng = rng
 
     def update(self, contexts, items, outcomes, iteration, plot, figsize, fontsize, name):
         pass
 
-
 class PyTorchLogisticRegressionAllocator(Allocator):
-    """ An allocator that estimates P(click) with Logistic Regression implemented in PyTorch"""
-
     def __init__(self, rng, embedding_size, num_items, thompson_sampling=True):
         self.response_model = PyTorchLogisticRegression(n_dim=embedding_size, n_items=num_items)
         self.thompson_sampling = thompson_sampling
         super(PyTorchLogisticRegressionAllocator, self).__init__(rng)
 
     def update(self, contexts, items, outcomes, iteration, plot, figsize, fontsize, name):
-        # Rename
         X, A, y = contexts, items, outcomes
 
         if len(y) < 2:
             return
 
-        # Fit the model
         self.response_model.train()
         epochs = 8192 * 2
         lr = 2e-3
@@ -43,10 +35,10 @@ class PyTorchLogisticRegressionAllocator(Allocator):
         X, A, y = torch.Tensor(X), torch.LongTensor(A), torch.Tensor(y)
         losses = []
         for epoch in tqdm(range(int(epochs)), desc=f'{name}'):
-            optimizer.zero_grad()  # Setting our stored gradients equal to zero
+            optimizer.zero_grad()
             loss = self.response_model.loss(torch.squeeze(self.response_model.predict_item(X, A)), y)
-            loss.backward()  # Computes the gradient of the given tensor w.r.t. the weights/bias
-            optimizer.step()  # Updates weights and biases with the optimizer (SGD)
+            loss.backward()
+            optimizer.step()
             losses.append(loss.item())
             scheduler.step(loss)
 
@@ -54,7 +46,6 @@ class PyTorchLogisticRegressionAllocator(Allocator):
                 print(f'Stopping at Epoch {epoch}')
                 break
 
-        # Laplace Approximation for variance q
         with torch.no_grad():
             for item in range(self.response_model.m.shape[0]):
                 item_mask = items == item
@@ -67,10 +58,7 @@ class PyTorchLogisticRegressionAllocator(Allocator):
     def estimate_CTR(self, context, sample=True):
         return self.response_model(torch.from_numpy(context.astype(np.float32)), sample=(self.thompson_sampling and sample)).detach().numpy()
 
-
 class OracleAllocator(Allocator):
-    """ An allocator that acts based on the true P(click)"""
-
     def __init__(self, rng):
         self.item_embeddings = None
         super(OracleAllocator, self).__init__(rng)
@@ -80,4 +68,3 @@ class OracleAllocator(Allocator):
 
     def estimate_CTR(self, context):
         return sigmoid(self.item_embeddings @ context)
-
